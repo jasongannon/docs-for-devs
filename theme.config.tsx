4ceedf036880bf4bf3e6ef64bc306a4d26ffbab6
Jason Gannon <102169615+jasgannon@users.noreply.github.com>
2023-09-20 09:12:16 -0400

    init-commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Docs for Devs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/jasongannon/docs-for-devs',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  //chat: {
+    //link: 'https://discord.com',
+  //},
+  docsRepositoryBase: 'https://github.com/jasongannon/docs-for-devs/blob/main',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Docs for Devs',
   },
 }
 
